0360700ac4ff9fca6719d9e2f8d14c0ac086273c
isaac <nodemadic@gmail.com>
2023-03-01 18:50:10 -0500

    Sets up easier additions of multiple commands

--- bot.py
+++ bot.py
@@ -1,17 +1,42 @@
 import discord
 import requests
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from the .env file
+load_dotenv()
 
 client = discord.Client()
 
-API_ENDPOINT = "API_ENDPOINT HERE"
+API_ENDPOINTS = {
+    '/trump': 'https://api.elevenlabs.io/v1/text-to-speech/ws3k037rKhKuz8MEyprg/stream',
+    '/biden': 'https://api.elevenlabs.io/v1/text-to-speech/IlHFNE62tMNgCbPKR5Xs/stream',
+    '/shinzo': 'https://api.elevenlabs.io/v1/text-to-speech/SHINZO/stream'
+}
+
+# Get the Discord bot token and API key from the environment variables
+DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+API_KEY = os.getenv('API_KEY')
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!play'):
-        # Get the phrase from the user input
-        phrase = message.content.split('!play ')[1]
+    if message.content.startswith('.'):
+        # Get the command and phrase from the user input
+        parts = message.content.split()
+        command = parts[0]
+        phrase = ' '.join(parts[1:])
 
-        # Send a request to the API endpoint with the phrase as a parameter
+        # Check if the command is valid
+        if command not in API_ENDPOINTS:
+            return
+
+        # Send a request to the API endpoint
+        api_endpoint = API_ENDPOINTS[command]
+        headers = {
+            'accept': '*/*',
+            'xi-api-key': API_KEY,
+            'Content-Type': 'application/json'
+        }
         request_data = {
             "text": phrase,
             "voice_settings": {
@@ -19,7 +44,7 @@ async def on_message(message):
                 "similarity_boost": 0
             }
         }
-        response = requests.post(API_ENDPOINT, json=request_data)
+        response = requests.post(api_endpoint, headers=headers, json=request_data)
 
         # Check if the response was successful
         if response.ok:
@@ -36,4 +61,4 @@ async def on_message(message):
             error_message = response.json()["detail"][0]["msg"]
             print(f"Error: {error_message}")
 
-client.run('YOUR_BOT_TOKEN')
+client.run(DISCORD_BOT_TOKEN)
